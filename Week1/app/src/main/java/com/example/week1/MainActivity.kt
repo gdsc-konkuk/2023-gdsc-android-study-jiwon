ff4f956d040702979b567383b4583dca2baba81f
parkjiwon <ji11won@naver.com>
2023-10-30 13:26:21 +0900

    Feat : MainActivity 구현

--- Week1/app/src/main/java/com/example/week1/MainActivity.kt
+++ Week1/app/src/main/java/com/example/week1/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.example.week1
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.week1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.editMyProfileIv.setOnClickListener {
+            val intent: Intent = Intent(this, EditActivity::class.java)
+            intent.putExtra("nickname", binding.nicknameTv.text)
+            intent.putExtra("email", binding.emailTv.text)
+            startActivity(intent)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(resultCode == Activity.RESULT_OK) {
+            binding.nicknameTv.text = data?.getStringExtra("nickname")
+        }
     }
 }
