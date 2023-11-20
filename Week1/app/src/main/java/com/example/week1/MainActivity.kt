4244d1af51760431e90c36dd40ba3fa425886b44
parkjiwon <ji11won@naver.com>
2023-11-20 15:28:32 +0900

    Feat : #7 RoomDB 초기화
    객체 생성 및 초기값 설정

--- Week1/app/src/main/java/com/example/week1/MainActivity.kt
+++ Week1/app/src/main/java/com/example/week1/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import com.example.week1.databinding.ActivityMainBinding
+import com.example.week1.db.User
+import com.example.week1.db.UserRoomDatabase
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,6 +16,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val userDatabase = UserRoomDatabase.getDatabase(this)
+        initDatabase(userDatabase)
 
         replaceFragment(supportFragmentManager.beginTransaction(), HomeFragment())
 
@@ -41,6 +46,15 @@ class MainActivity : AppCompatActivity() {
             val fragmentTransaction = supportFragmentManager.beginTransaction()
             replaceFragment(fragmentTransaction, CreateFragment())
         }
+    }
+
+    private fun initDatabase(userDatabase: UserRoomDatabase) {
+        val userDao = userDatabase.userDao()
+        val users = userDao.getAll()
+        if (!users.isNullOrEmpty()) return
+        userDao.insertUser(
+            User(name = "박지원", imageUrl = "https://images.unsplash.com/photo-1691425266385-31dfd2603cca?ixid=M3w0MTIxNzd8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MDA0MjgzNDN8&ixlib=rb-4.0.3")
+        )
     }
 
     private fun replaceFragment(fragmentTransaction: FragmentTransaction, fragment: Fragment) {
