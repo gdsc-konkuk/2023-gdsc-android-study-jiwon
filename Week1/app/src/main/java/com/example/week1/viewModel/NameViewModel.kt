726c4e58b3cb5a532fbb675ed9458ea1d3764659
parkjiwon <ji11won@naver.com>
2023-11-20 15:27:05 +0900

    Feat : #7 Name -> User ViewModel 변경
    이름만 관리하던 NameViewModel을 이름과 프로필 이미지를 관리하는 UserViewModel로 변경

--- Week1/app/src/main/java/com/example/week1/viewModel/NameViewModel.kt
+++ Week1/app/src/main/java/com/example/week1/viewModel/NameViewModel.kt
@@ -3,18 +3,28 @@ package com.example.week1.viewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.example.week1.ToDoData
 
 class NameViewModel: ViewModel() {
     private val _currentName: MutableLiveData<String> by lazy {
         MutableLiveData<String>()
     }
 
-    //    이거 대신에 fun getToDoList(): LiveData<List<ToDoData>>로 만드는 것은 어떨까?
     val currentName: LiveData<String>
         get() = _currentName
 
+
+    private val _currentImage: MutableLiveData<String> by lazy {
+        MutableLiveData<String>()
+    }
+
+    val currentImage: LiveData<String>
+        get() = _currentImage
+
     fun setName(newName: String) {
         _currentName.value = newName
+    }
+
+    fun setImage(newImage: String) {
+        _currentImage.value = newImage
     }
 }
---
+++ Week1/app/src/main/java/com/example/week1/viewModel/UserViewModel.kt
@@ -0,0 +1,20 @@
+package com.example.week1.viewModel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.week1.db.User
+
+class UserViewModel: ViewModel() {
+
+    private val _currentUser: MutableLiveData<User> by lazy {
+        MutableLiveData<User>()
+    }
+
+    val currentUser: LiveData<User>
+        get() = _currentUser
+
+    fun setUser(user: User) {
+        _currentUser.value = user
+    }
+}
