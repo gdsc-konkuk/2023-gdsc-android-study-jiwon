dfa63e6b04dd5551898e8d8791fc7a131cb908f7
parkjiwon <ji11won@naver.com>
2023-11-26 21:12:22 +0900

    Feat : #9 뒤로가기, todo생성 dialog 추가

--- Week1/app/src/main/java/com/example/week1/MainActivity.kt
+++ Week1/app/src/main/java/com/example/week1/MainActivity.kt
@@ -2,20 +2,31 @@ package com.example.week1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import com.example.week1.databinding.ActivityMainBinding
 import com.example.week1.db.User
 import com.example.week1.db.UserRoomDatabase
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ExitDialog.ExitInterface {
 
     private lateinit var binding: ActivityMainBinding
+
+    private val onBackPressedCallback = object : OnBackPressedCallback(true) {
+        override fun handleOnBackPressed() {
+            val dialog = ExitDialog(this@MainActivity)
+            dialog.isCancelable = false
+            dialog.show(this@MainActivity.supportFragmentManager, "ConfirmDialog")
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        this.onBackPressedDispatcher.addCallback(this, onBackPressedCallback)
 
         val userDatabase = UserRoomDatabase.getDatabase(this)
         initDatabase(userDatabase)
@@ -43,8 +54,10 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.ibtnTodoPlus.setOnClickListener {
-            val fragmentTransaction = supportFragmentManager.beginTransaction()
-            replaceFragment(fragmentTransaction, CreateFragment())
+            /*val fragmentTransaction = supportFragmentManager.beginTransaction()
+            replaceFragment(fragmentTransaction, CreateFragment())*/
+            val createTodoBottomSheet = CreateFragment()
+            createTodoBottomSheet.show(supportFragmentManager, CreateFragment.TAG)
         }
     }
 
@@ -60,5 +73,9 @@ class MainActivity : AppCompatActivity() {
     private fun replaceFragment(fragmentTransaction: FragmentTransaction, fragment: Fragment) {
         fragmentTransaction.replace(R.id.fl_content, fragment)
             .commit()
+    }
+
+    override fun exitBtnClick() {
+        finish()
     }
 }
